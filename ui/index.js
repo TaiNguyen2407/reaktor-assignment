86e3d1c1741670328d8ad4fda23354cbc94d2888
Tai Nguyen <phatn@metropolia.fi>
2022-12-21 15:49:07 +0200

    implementing disappearing after 10 mins features

--- ui/index.js
+++ ui/index.js
@@ -8,52 +8,72 @@ const nestOriginX = 250000;
 const nestOriginY = 250000;
 const nestRadius = 100000;
 
-const fetchData = async() => {
-        const response = await fetch(url + '/drones');
-        const xml = await response.text();
-        const result = JSON.parse(xml);
-        let count = 0;
-        let violated = [];
-        const drones = result.report.capture[0].drone;
-        drones.forEach(drone => {
-            const droneSerialNumber = drone.serialNumber;
-            const droneX = drone.positionX;
-            const droneY = drone.positionY;
+// const fetchData = setInterval(async() => {
+//     const response = await fetch(url + '/drones');
+//         const xml = await response.text();
+//         const result = JSON.parse(xml);
+//         let count = 0;
+//         let violated = [];
+//         const drones = result.report.capture[0].drone;
+//         drones.forEach(drone => {
+//             const droneSerialNumber = drone.serialNumber;
+//             const droneX = drone.positionX;
+//             const droneY = drone.positionY;
 
-            const violatedDroneDiv = document.createElement('div');
-            const violatedDroneSerialNumber = document.createElement('p');
-            const violatedDistance = document.createElement('p');
+//             const violatedDroneDiv = document.createElement('div');
+//             const violatedDrones = document.createElement('h4');
             
 
-            const droneViolated = Math.sqrt((Math.pow((nestOriginX - droneX),2)) + (Math.pow((nestOriginY - droneY),2)));
+//             const droneDistance = Math.sqrt((Math.pow((nestOriginX - droneX),2)) + (Math.pow((nestOriginY - droneY),2)));
 
 
-            if (droneViolated <= nestRadius){
-                violatedDroneSerialNumber.innerHTML = 'Drone number:' + droneSerialNumber;
-                violatedDistance.innerHTML = 'Distance to nest: ' + roundNumber(droneViolated, 2);
+//             if (droneDistance <= nestRadius){
+//                 violatedDrones.innerHTML = 'Drone number: ' + droneSerialNumber + ' and Distance to nest: ' + roundNumber(droneDistance, 2);
+//                 fetchPilotInfo(droneSerialNumber);
+//                 count++;
+//             }
 
+//             violatedDroneDiv.appendChild(violatedDrones);
+//             test.appendChild(violatedDroneDiv);
+//         });   
+// }, 2000);
 
-                fetchPilotInfo(droneSerialNumber);
-                count++;
+// const fetchData = async() => {
+//         const response = await fetch(url + '/drones');
+//         const xml = await response.text();
+//         const result = JSON.parse(xml);
+//         let count = 0;
+//         let violated = [];
+//         const drones = result.report.capture[0].drone;
+//         drones.forEach(drone => {
+//             const droneSerialNumber = drone.serialNumber;
+//             const droneX = drone.positionX;
+//             const droneY = drone.positionY;
 
-            }
-
-            violatedDroneDiv.appendChild(violatedDroneSerialNumber);
-            violatedDroneDiv.appendChild(violatedDistance);
-            test.appendChild(violatedDroneDiv);
+//             const violatedDroneDiv = document.createElement('div');
+//             const violatedDrones = document.createElement('p');
             
-        });   
+
+//             const droneDistance = Math.sqrt((Math.pow((nestOriginX - droneX),2)) + (Math.pow((nestOriginY - droneY),2)));
+
+
+//             if (droneDistance <= nestRadius){
+//                 violatedDrones.innerHTML = 'Drone number:' + droneSerialNumber + ' \n and Distance to nest: ' + roundNumber(droneDistance, 2);
+//                 fetchPilotInfo(droneSerialNumber);
+//                 count++;
+//             }
+
+//             violatedDroneDiv.appendChild(violatedDrones);
+//             test.appendChild(violatedDroneDiv);
+//         });   
         
-        console.log(violated);
-        console.log(count);
-}
+// }
 
-fetchData();
+// fetchData;
 
 const fetchPilotInfo = async(serialNumber) => {
     const response = await fetch(url + '/pilots/' + serialNumber);
     const pilot = await response.json();
-    console.log(pilot);
 
     //Create HTML with DOM element
     const pilotInfo = document.createElement('div');
@@ -64,7 +84,7 @@ const fetchPilotInfo = async(serialNumber) => {
 
 
     //Assign values to HTML elements
-    pilotSerial.innerHTML = serialNumber;
+    pilotSerial.innerHTML = 'Serial Number: ' + serialNumber;
     pilotName.innerHTML = 'pilot name: ' + pilot.firstName + ' ' + pilot.lastName;
     pilotEmail.innerHTML = 'pilot email: ' + pilot.email;
     pilotPhoneNumber.innerHTML = 'pilot phone number: ' + pilot.phoneNumber;
@@ -88,10 +108,3 @@ const roundNumber = (value, decimals) => {
 }
 
 
-// const updateAfter2Seconds = () => {
-//     setInterval(() => {
-//         window.location.reload();
-//     }, 2000);
-// }
-
-// updateAfter2Seconds();
